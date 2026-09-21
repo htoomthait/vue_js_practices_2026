34b1844ec7243c2168da1bd8bbf785259551c3e0
Htoo Maung Thait <htoomaungthait@gmail.com>
2026-09-21 15:01:36 +0630

    nuxt crash course: reusable

---
+++ nuxt-dojo/app/components/ProductCard.vue
@@ -0,0 +1,18 @@
+<template>
+   <div class="card text-center flex flex-col justify-end">
+         
+         <img :src="`${product.image}`" alt="product thumb" class="thumb" />
+         <p class="font-bold text-grey-500 m-4 truncate">{{product.title}}</p>
+         <NuxtLink :to="`/products/${product.id}`"> <p class="btn my-4">view detail</p> </NuxtLink>
+
+    </div>
+</template>
+
+<script setup>
+    const { product } = defineProps(['product'])
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
