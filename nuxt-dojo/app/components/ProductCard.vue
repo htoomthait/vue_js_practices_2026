96838f73abc4590c86516c5f8e86b750956dcec2
Htoo Maung Thait <htoomaungthait@gmail.com>
2026-09-21 15:47:59 +0630

    nuxt crash course: error page

--- nuxt-dojo/app/components/ProductCard.vue
+++ nuxt-dojo/app/components/ProductCard.vue
@@ -1,7 +1,7 @@
 <template>
-   <div class="card text-center flex flex-col justify-end">
+   <div class="card text-center flex flex-col justify-end ">
          
-         <img :src="`${product.image}`" alt="product thumb" class="thumb" />
+         <img :src="`${product.image}`" alt="product thumb" class="w-[50%] mx-auto" />
          <p class="font-bold text-grey-500 m-4 truncate">{{product.title}}</p>
          <NuxtLink :to="`/products/${product.id}`"> <p class="btn my-4">view detail</p> </NuxtLink>
 
