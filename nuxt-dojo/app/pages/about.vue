8836e0db017ffd86648548b80db7b72741954fd5
Htoo Maung Thait <htoomaungthait@gmail.com>
2026-09-21 16:50:12 +0630

    nuxt crush course: server routes

--- nuxt-dojo/app/pages/about.vue
+++ nuxt-dojo/app/pages/about.vue
@@ -3,11 +3,14 @@
         <h2>About</h2>
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio debitis, ipsa, quia soluta, sed quo praesentium earum cumque nostrum similique laborum illum sint! Possimus, voluptas. Laboriosam quae neque doloribus esse.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit odio neque pariatur amet? Sit, tempore perspiciatis aperiam tempora quisquam ut veniam est minima ex aliquid corrupti quis facilis ad veritatis?</p>
+        <div>{{data}}</div>
     </div>
 </template>
 
 <script setup>
-
+    const {data} = await useFetch('/api/ninjas', {
+        method: 'post'
+    })
 </script>
 
 <style lang="scss" scoped>
